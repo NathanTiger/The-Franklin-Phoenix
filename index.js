9d27a72c28107ed3800b698f9e3bdb778152f0a3
NathanTiger <yg0@yahoo.com>
2024-08-13 18:57:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-// side_menu is the menu.
-const side_menu=document.getElementById('side_menu');
-// menu is the menu button.
-const menu_button=document.getElementById('menu_button');
-const go_back=document.getElementById('go_back');
-if(menu_button && go_back){
-menu_button.addEventListener('click',()=>{
-    side_menu.style.width='250px';
+document.addEventListener('DOMContentLoaded',function(){
+
+
+    // side_menu is the menu.
+    const side_menu=document.getElementById('side_menu');
+    // menu is the menu button.
+    const menu_button=document.getElementById('menu_button');
+    const go_back=document.getElementById('go_back');
+    menu_button.addEventListener('click',()=>{
+        if(side_menu.classList.contains('open')){
+            side_menu.classList.remove('open')
+        }else{
+            side_menu.classList.add()
+        }
+    })
+    
 })
-go_back.addEventListener('click',()=>{
-    side_menu.style.width='0px';
-})
-}
