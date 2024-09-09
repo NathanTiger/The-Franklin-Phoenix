dd5876f817e70ce7889809ccba1001073351b4b8
NathanTiger <yg0@yahoo.com>
2024-09-09 17:14:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded',function(){
     const side_menu=document.getElementById('side_menu');
     // menu is the menu button.
     const menu_button=document.getElementById('menu_button');
+    const franklin_button=document.getElementById('franklin_issues');
+    const about_us_button=document.getElementById('about_us');
     const go_back=document.getElementById('go_back');
     menu_button.addEventListener('click',()=>{
         if(side_menu.classList.contains('open')){
@@ -13,5 +15,13 @@ document.addEventListener('DOMContentLoaded',function(){
             side_menu.classList.add('open')
         }
     })
-    
+    go_back.addEventListener('click',()=>{
+        side_menu.classList.remove('open')
+    })
+    franklin_button.addEventListener('click',()=>{
+
+    })
+    about_us_button.addEventListener('click',()=>{
+        window.location.href='aboutUs.html';
+    })
 })
